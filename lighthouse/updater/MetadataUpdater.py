c6dcb4d8e321d4e024b728539004f8b293fa8cd4
Jack <jack@lbry.io>
2016-09-12 13:30:47 -0400

    fix claim updates, delay name recheck

--- lighthouse/updater/MetadataUpdater.py
+++ lighthouse/updater/MetadataUpdater.py
@@ -84,6 +84,11 @@ class MetadataUpdater(object):
         claim_value = base64.b64encode(claim['value'])
         nout = claim['nOut']
         claim_id = claim['claimId']
+        try:
+            self._claims.update({txid: {'name': name, 'claim_id': claim_id, 'nout': nout, 'metadata': Metadata(json.loads(claim['value']), process_now=False)}})
+        except AssertionError:
+            self._claims.update({txid: {'name': name, 'claim_id': claim_id, 'nout': nout}})
+
         log.info("Add claim for lbry://%s, id: %s, tx: %s", name, claim_id, txid)
 
         d = self.db.runQuery("insert into blocks values (?, ?, ?)", (height, block_hash, txid))
@@ -94,7 +99,7 @@ class MetadataUpdater(object):
     def _load_stream_size(self, sd_hash):
         def _save(size):
             if size:
-                log.info("Load size for %s", sd_hash)
+                log.debug("Load size for %s", sd_hash)
                 self.size_cache[sd_hash] = size
             if not self.size_cache.get(sd_hash, False) and self.sd_attempts.get(sd_hash, 0) < MAX_SD_TRIES and sd_hash not in self.descriptors_to_download:
                 self.descriptors_to_download.append(sd_hash)
@@ -108,7 +113,7 @@ class MetadataUpdater(object):
     def _load_sd_attempts(self, sd_hash):
         def _save(attempts):
             if attempts:
-                log.info("Loaded %i attempts for %s", attempts, sd_hash)
+                log.debug("Loaded %i attempts for %s", attempts, sd_hash)
                 self.sd_attempts[sd_hash] = attempts
             return defer.succeed(True)
 
@@ -127,20 +132,20 @@ class MetadataUpdater(object):
             return d
 
         def _save(claim_id, name, tx, n, encoded_meta):
-            self._claims.update({tx: {'name': name, 'claim_id': claim_id, 'nout': n}})
             try:
                 decoded = json.loads(base64.b64decode(encoded_meta))
                 verify_name_characters(name)
                 meta = Metadata(decoded, process_now=False)
                 ver = meta.get('ver', '0.0.1')
-                log.info("lbry://%s conforms to metadata version %s" % (name, ver))
+                log.debug("lbry://%s conforms to metadata version %s" % (name, ver))
                 self._claims.update({tx: {'name': name, 'claim_id': claim_id, 'nout': n, 'metadata': meta}})
                 sd_hash = meta['sources']['lbry_sd_hash']
                 d = self._load_sd_attempts(sd_hash)
                 d.addCallback(lambda _: self._load_stream_size(sd_hash))
                 return d
             except:
-                return self._notify_bad_metadata(txid)
+                self._claims.update({txid: {'name': name, 'claim_id': claim_id, 'nout': n}})
+                return self._notify_bad_metadata(name, txid)
 
         d = self.db.runQuery("select * from claims where txid=?", (txid,))
         d.addCallback(lambda r: r[0])
@@ -168,7 +173,7 @@ class MetadataUpdater(object):
         if height % 100 == 0:
             bps = 100/float(time.time() - self._last_time)
             self._last_time = time.time()
-            log.info("Imported %i blocks, %f blocks/s", height, bps)
+            log.debug("Imported %i blocks, %f blocks/s", height, bps)
 
         d = defer.DeferredList([self._add_claim(block_hash, height, name, claim, tx) for (name, claim, tx) in to_add])
         d.addCallback(lambda _: self.db.runQuery("select * from blocks where block_height=?", (height,)))
@@ -178,13 +183,13 @@ class MetadataUpdater(object):
     def _catch_up_claims(self, cache_height):
         chain_height = self.api.get_block({'blockhash': self.api.get_best_blockhash()})['height']
         if chain_height > cache_height:
+            log.info("Catching up with blockchain")
             d = self._add_claims_for_height(cache_height + 1)
-            d.addCallback(lambda _: reactor.callLater(0, self.catchup))
+            d.addCallback(lambda _: reactor.callLater(1, self.catchup))
         else:
             if not self._is_running:
                 self._is_running = True
                 self._start()
-
             d = defer.succeed(None)
             d.addCallback(lambda _: reactor.callLater(60, self.catchup))
 
@@ -194,16 +199,21 @@ class MetadataUpdater(object):
         for c in nametrie:
             name = c['name']
             txid = c['txid']
-            claim = self._claims[txid]
+            claim = self._claims.get(txid, {})
             if 'metadata' in claim:
                 try:
                     meta = Metadata(claim['metadata'], process_now=False)
                     self.metadata[name] = meta
                 except AssertionError:
                     log.info("Bad metadata for lbry://%s", name)
-        return defer.succeed(None)
+            else:
+                log.debug("Missing metadata for lbry://%s", name)
 
     def refresh_winning_name(self):
+        def _delayed_add(name):
+            log.info("Checking lbry://%s", name)
+            self._claims_to_check.append(name)
+
         if len(self._claims_to_check):
             name = self._claims_to_check.pop()
             log.info("Checking winning claim for lbry://%s", name)
@@ -212,8 +222,9 @@ class MetadataUpdater(object):
                 try:
                     self.metadata[name] = self._claims[current['txid']]['metadata']
                     self.claimtrie[name] = self._claims[current['txid']]
+                    log.info("Updated lbry://%s", name)
                 except KeyError:
-                    self._claims_to_check.append(name)
+                    reactor.callLater(30, _delayed_add, name)
             reactor.callLater(1, self.refresh_winning_name)
         else:
             reactor.callLater(30, self.refresh_winning_name)
@@ -244,8 +255,8 @@ class MetadataUpdater(object):
         d = self.db.runQuery("delete from stream_size where sd_hash=?", (sd_hash,))
         d.addCallback(lambda _: self.db.runQuery("insert into stream_size values (?, ?)", (sd_hash, total_bytes)))
 
-    def _notify_bad_metadata(self, txid):
-        # log.info("claim tx %s does not conform to any specification", txid)
+    def _notify_bad_metadata(self, name, txid):
+        log.info("claim for lbry://%s does not conform to any specification", name)
         if txid not in self.bad_uris:
             self.bad_uris.append(txid)
         return defer.succeed(True)
@@ -257,7 +268,6 @@ class MetadataUpdater(object):
             d.addCallback(lambda _: reactor.callLater(1, self.update_descriptors))
         else:
             reactor.callLater(30, self.update_descriptors)
-        return defer.succeed(None)
 
     def _update_costs(self):
         d = defer.DeferredList([self._get_cost(n) for n in self.metadata], consumeErrors=True)
@@ -287,15 +297,19 @@ class MetadataUpdater(object):
     def catchup(self):
         d = self._get_cached_height()
         d.addCallback(self._catch_up_claims)
+        return d
 
     def _start(self):
-        d = self.update_descriptors()
-        d.addCallback(lambda _: self.refresh_winning_name())
-        d.addCallback(lambda _: self._initialize_metadata())
-        d.addCallback(lambda _: self.exchange_rate_manager.start())
-        d.addCallback(lambda _: self.cost_updater.start(60))
-        d.addCallback(lambda _: self.name_refresher.start(300))
-        d.addCallback(lambda _: log.info("started!"))
+        self.update_descriptors()
+        self._initialize_metadata()
+        self.refresh_winning_name()
+        self.exchange_rate_manager.start()
+        self.cost_updater.start(60)
+        log.info("*********************")
+        log.info("Loaded %i names", len(self.metadata))
+        log.info("Loaded %i claims", len(self._claims))
+        log.info("Started!")
+        log.info("*********************")
 
     def start(self):
         d = self._open_db()
@@ -303,6 +317,7 @@ class MetadataUpdater(object):
         d.addCallback(lambda _: self.catchup())
 
     def stop(self):
+        log.info("*********************")
         log.info("Stopping updater")
         if self.cost_updater.running:
             self.cost_updater.stop()
